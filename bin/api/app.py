8f924551e2ce30900a18794900fcb6c007b5a2c1
Mattia Braga <mattiabraga2006@gmail.com>
2025-02-01 18:34:53 +0100

    updates req

--- bin/api/app.py
+++ bin/api/app.py
@@ -1,26 +1,26 @@
-import flask
-from flask_cors import CORS
-from flask import request, jsonify, render_template
+from fastapi import FastAPI, Request, Form
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from movies import get_similar, get_recommendations
 import pandas as pd
 
-app = flask.Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:8010"}})
+app = FastAPI()
 
-'''
-@app.route('/')
-def home():
-    return render_template('index.html')
-'''
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://film.figliolo.it"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.route('/similar', methods=['POST'])
-def similar():
-    title = request.form['title']
+@app.post("/similar")
+async def similar(request: Request, title: str = Form(...)):
     recommendations = get_recommendations(title)
     if isinstance(recommendations, pd.Series):
         recommendations = recommendations.tolist()
-    return jsonify(recommendations)
+    return JSONResponse(content=recommendations)
 
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8011)
